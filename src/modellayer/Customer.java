6f14e741bcc5a38bf3ce346e9cabedd117387eb5
Serec <soren.kaae@gmail.com>
2015-03-19 09:14:33 +0100

    getters and setters

--- src/modellayer/Customer.java
+++ src/modellayer/Customer.java
@@ -12,6 +12,46 @@ public class Customer {
 	{
 		
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
 	
 //	public Customer()
 	
