6f93d5fc95f0769f0444fc1ce217f098398bddfb
Serec <soren.kaae@gmail.com>
2015-03-19 09:13:10 +0100

    customer still incomplete

--- src/modellayer/Customer.java
+++ src/modellayer/Customer.java
@@ -3,6 +3,18 @@ package modellayer;
 public class Customer {
 
 	private String name;
+	private String address;
+	private String zipCode;
+	private String city;
+	private String phoneNo;
+	
+	public Customer()
+	{
+		
+	}
+	
+//	public Customer()
+	
 	
 	
 }
